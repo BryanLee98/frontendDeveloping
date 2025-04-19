fc36df51050fd05d35aa6aeb7d6be953f72654d1
BryanLee98 <153425328+BryanLee98@users.noreply.github.com>
2025-04-19 18:57:20 +0900

    Delete src/app/01_boards directory

--- src/app/01_boards/[boardID]/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import PAGE_DETAIL_COMPO from "@/app/components/boards-detail"
-
-const DetailPage = () => {
-  return <PAGE_DETAIL_COMPO />
-}
-
-export default DetailPage
--- src/app/01_boards/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import PAGE_LIST_COMPO from "../components/boards-list"
-
-const PageList = () => {
-  return <PAGE_LIST_COMPO />
-}
-
-export default PageList
