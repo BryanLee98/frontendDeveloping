8a796b638a8ab9712195b83ef3b7c65cb43d188c
BryanLee98 <153425328+BryanLee98@users.noreply.github.com>
2025-04-20 00:04:06 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
+    async rewrites() {
+    return [
+      {
+        source: "http://localhost:3000",
+        destination: `https://marketpageproject.vercel.app`,
+      },
+    ]
+  },
+  trailingSlash: true,
 }
 
 export default nextConfig
