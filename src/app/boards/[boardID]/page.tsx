5172ee3fcd23ed02f9c30b3d4df7469b02df0781
BryanLee98 <153425328+BryanLee98@users.noreply.github.com>
2025-04-19 19:01:01 +0900

    Add files via upload

--- src/app/boards/[boardID]/page.tsx
+++ src/app/boards/[boardID]/page.tsx
@@ -8,7 +8,19 @@ const DetailPage = () => {
   return (
     <>
       <PAGE_DETAIL_COMPO />
-      <COMMENT_WRITE_COMPO />
+      <COMMENT_WRITE_COMPO
+        isEdit={false}
+        el={{
+          __typename: undefined,
+          _id: "",
+          writer: undefined,
+          contents: "",
+          rating: 0,
+          createdAt: undefined,
+          updatedAt: undefined,
+        }}
+        setIsEdit={() => {}}
+      />
       <COMMENT_LIST_COMPO />
     </>
   )
