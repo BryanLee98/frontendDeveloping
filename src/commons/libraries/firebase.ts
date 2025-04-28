80c38879a3eab08ec2e6f93a3e0153723aeab424
BryanLee98 <153425328+BryanLee98@users.noreply.github.com>
2025-04-28 21:33:21 +0900

    Update firebase.ts

--- src/commons/libraries/firebase.ts
+++ src/commons/libraries/firebase.ts
@@ -1,12 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"
-import { config } from "../../../api.keys"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: `${config.KEY}`,
+  apiKey: process.env.API_KEY,
   authDomain: "marketpage-96fb4.firebaseapp.com",
   projectId: "marketpage-96fb4",
   storageBucket: "marketpage-96fb4.firebasestorage.app",
