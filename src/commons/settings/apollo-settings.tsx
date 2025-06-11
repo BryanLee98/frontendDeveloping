43d8d658034cc1693bf9fe52df8b0b39c675a26f
BryanLee98 <153425328+BryanLee98@users.noreply.github.com>
2025-06-11 18:29:02 +0900

    Add files via upload

--- src/commons/settings/apollo-settings.tsx
+++ src/commons/settings/apollo-settings.tsx
@@ -2,37 +2,18 @@
 
 import { ApolloClient, ApolloLink, ApolloProvider, InMemoryCache } from "@apollo/client"
 import createUploadLink from "apollo-upload-client/createUploadLink.mjs"
-import { use, useEffect } from "react"
-import { useAccessToken } from "../store/accessToken_stores/stores"
 
 interface IApolloSetting {
   children: React.ReactNode
 }
-
-//캐시가 초기화 되지 않기 위해서
-const GLOBAL_STATE = new InMemoryCache()
-
-type AccessTokenStore = {
-  accessToken: string
-  setAccessToken: (accessToken: string) => void
-}
-
-export default function ApolloHeaderSetting(props: IApolloSetting) {
-  const { accessToken, setAccessToken } = useAccessToken() as AccessTokenStore
-
-  useEffect(() => {
-    setAccessToken(localStorage.getItem("accessToken") ?? "")
-  }, [])
+export default function ApolloUploadSetting(props: IApolloSetting) {
   const uploadLink = createUploadLink({
     uri: "http://main-practice.codebootcamp.co.kr/graphql",
-    headers: {
-      Authorization: `Bearer ${accessToken} `,
-    },
   })
 
   const client = new ApolloClient({
     link: ApolloLink.from([uploadLink]),
-    cache: GLOBAL_STATE,
+    cache: new InMemoryCache(),
   })
 
   return <ApolloProvider client={client}>{props.children}</ApolloProvider>
