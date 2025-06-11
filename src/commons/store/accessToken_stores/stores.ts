b39663de2758ce5bd47b6a8557bf83dcc6e10f5b
BryanLee98 <153425328+BryanLee98@users.noreply.github.com>
2025-06-11 18:29:22 +0900

    Delete src/commons/store/accessToken_stores directory

--- src/commons/store/accessToken_stores/stores.ts
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand"
-
-export const useAccessToken = create((set) => ({
-  accessToken: "",
-  setAccessToken: (newToken: string) =>
-    set(() => ({
-      accessToken: newToken,
-    })),
-}))
