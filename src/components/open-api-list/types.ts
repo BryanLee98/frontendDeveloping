30f6a42923d738ae3545f346c435fd4ef5341611
BryanLee98 <bryan7845gmail.com>
2025-06-16 18:44:43 +0900

    고양이 페이지 삭제

--- src/components/open-api-list/types.ts
+++
@@ -1,9 +0,0 @@
-import { DocumentData } from "firebase/firestore"
-
-export interface IOpenApiPageProps {
-  keyword: string
-  data?: any
-  dataList?: DocumentData[]
-  setDataList?: React.Dispatch<React.SetStateAction<DocumentData[]>>
-  currentData?: DocumentData[]
-}
